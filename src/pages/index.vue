92d5a656598c6e9e1b4017002c3fa42baa687d34
Taketo Yoshida <tamanyan.sss@gmail.com>
2019-02-05 00:19:26 +0700

    First commit

---
+++ src/pages/index.vue
@@ -0,0 +1,68 @@
+<template>
+  <section class="container">
+    <div>
+      <logo />
+      <h1 class="title">
+        Nuxt TypeScript Sample
+      </h1>
+      <h2 class="subtitle">
+        Nuxt TypeScript Project
+      </h2>
+      <div class="links">
+        <a
+          href="https://nuxtjs.org/"
+          target="_blank"
+          class="button--green"
+        >Documentation</a>
+        <a
+          href="https://github.com/nuxt/nuxt.js"
+          target="_blank"
+          class="button--grey"
+        >GitHub</a>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+import Logo from '../components/Logo.vue'
+
+@Component({
+  components: {
+    Logo,
+  }
+})
+export default class Index extends Vue {}
+</script>
+
+<style scoped>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  display: block;
+  font-weight: 300;
+  font-size: 50px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
